dc89585fe73f94b1c1429b4a1433bffd0b8a4329
Johan Falk <johan@krokedil.se>
2025-06-02 17:40:31 +0200

    Update helper-wc-blueprint-import.sh

--- commands/helper-wc-blueprint-import.sh
+++ commands/helper-wc-blueprint-import.sh
@@ -1,10 +1,10 @@
 # Helper script to import WooCommerce blueprint
 
 # Download the provided public JSON file to a private location in the home directory
-curl -fsSL "{{wc_blueprint_json_public_url}}" -o "$HOME/wc-blueprint-imported-from-instawp-command.json"
+curl -fsSL "{{wc_blueprint_json_public_url}}" -o "wc-blueprint-imported-from-instawp-command.json"
 
 # Import the WooCommerce blueprint using the wp wc command
-wp wc blueprint import "$HOME/wc-blueprint-imported-from-instawp-command.json" --show-messages=all --user=1
+wp wc blueprint import "wc-blueprint-imported-from-instawp-command.json" --show-messages=all --user=1
 
 # Remove the temporary file
-rm "$HOME/wc-blueprint-imported-from-instawp-command.json"
+rm "wc-blueprint-imported-from-instawp-command.json"
