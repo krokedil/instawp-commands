4e7d44701b2aa18ffba8be753c39a61169151208
Johan Falk <johan@krokedil.se>
2025-06-04 23:18:32 +0200

    Tweaks to setup-default-site script

--- commands/setup-default-site.sh
+++ commands/setup-default-site.sh
@@ -2,15 +2,18 @@
 
 # Install WooCommerce development version, which have support for blueprints. (Blueprints should be available as beta from WC 9.9)
 wp plugin install woocommerce --activate --force --skip-plugins --skip-themes --version=dev
+wp transient delete _wc_activation_redirect
 
 # Download WC blueprint JSON file, import it and delete it.
 curl -fsSL "https://raw.githubusercontent.com/krokedil/instawp-commands/refs/heads/main/assets/wc-blueprints/wc-blueprint-default.json" -o "wc-blueprint-imported-from-instawp-command.json"
 wp wc blueprint import "wc-blueprint-imported-from-instawp-command.json" --show-messages=all --user=1
 rm "wc-blueprint-imported-from-instawp-command.json"
 
-# Import products and coupons.
+# Download WXR xml, import products and coupons and delete it.
 wp plugin install wordpress-importer --activate
-wp import --url="https://raw.githubusercontent.com/krokedil/instawp-commands/refs/heads/main/assets/wxr-exports/products-coupons-for-tests.wordpress.xml" --authors=skip
+curl -fsSL "https://raw.githubusercontent.com/krokedil/instawp-commands/refs/heads/main/assets/wxr-exports/products-coupons-for-tests.wordpress.xml" -o "wxr-imported-from-instawp-command.xml"
+wp import wxr-imported-from-instawp-command.xml --authors=skip
+rm "wxr-imported-from-instawp-command.xml"
 
 # Create tax settings and shipping methods.
 wp wc tax create --country="SE" --rate="25" --name="VAT 25%" --class="standard" --user=1
@@ -36,10 +39,8 @@ wp user meta add 1 shipping_country "SE"
 wp user meta add 1 shipping_phone "+463390510"
 
 # Delete default pages, publish privacy policy and refund/returns pages and set refund/returns page as terms page.
-wp post delete $(wp post list --pagename='sample-page' --format=ids) --force # Delete defualt page "Sample page"
-wp post delete $(wp post list --post_name='hello-world' --format=ids) --force # Delete defualt post "Hello world!"
-wp post update $(wp post list --pagename='privacy-policy' --format=ids) --post_status=publish
-wp post update $(wp post list --pagename='refund_returns' --format=ids) --post_status=publish
+wp post delete $(wp post list --pagename='sample-page' --format=ids) $(wp post list --post_name='hello-world' --format=ids) --force # Delete defualt page "Sample page" and defualt post "Hello world!"
+wp post update $(wp post list --pagename='privacy-policy' --format=ids) $(wp post list --pagename='refund_returns' --format=ids) --post_status=publish
 wp option update woocommerce_terms_page_id $(wp post list --pagename='refund_returns' --format=ids)
 
 # Create and assign menus.
@@ -54,5 +55,4 @@ wp menu item add-post primary-menu $(wp option get woocommerce_myaccount_page_id
 # Configure theme and widgets.
 wp widget reset --all # Remove all default widgets to enable full width
 wp theme mod set storefront_product_pagination "0"
-wp option update show_on_front "page"
 wp option update page_on_front $(wp option get woocommerce_shop_page_id)
