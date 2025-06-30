f86195bd72db149f3740b2cc740f17d27f1765e9
Johan Falk <johan@krokedil.se>
2025-06-30 17:01:45 +0200

    Fix quoting in JSON string output in import script
    Changed double quotes to single quotes when echoing the wc_blueprint_json_string to ensure correct handling of the JSON string in the helper-wc-blueprint-import-json.sh script.

--- commands/helper-wc-blueprint-import-json.sh
+++ commands/helper-wc-blueprint-import-json.sh
@@ -1,7 +1,7 @@
 # Helper script to import a WooCommerce blueprint json that is defined as command values.
 
 # Write the provided json string to a private location in the home directory
-echo "{{wc_blueprint_json_string}}" > "wc-blueprint-imported-from-instawp-command.json"
+echo '{{wc_blueprint_json_string}}' > wc-blueprint-imported-from-instawp-command.json
 
 # Import the WooCommerce blueprint using the wp wc command
 wp wc blueprint import "wc-blueprint-imported-from-instawp-command.json" --show-messages=all --user=1
