3e80e9e9b2cbe4af09cb5f439e9ede43476f596c
Johan Falk <johan@krokedil.se>
2025-09-30 15:29:59 +0200

    Rename and improve patch options script
    Renamed helper-patch-option.sh to helper-patch-options.sh for clarity. Refactored the script to use a PATCH_OPTIONS_JSON environment variable and improved JSON parsing for better handling of option entries.

--- commands/helper-patch-options.sh
+++ commands/helper-patch-options.sh
@@ -2,9 +2,11 @@
 # The provided options should be in JSON format, for example:
 # {"option_1":{"key":"nested_option_1","key2":"nested_option_2"},"option_2":{"key":"nested_option_1","key2":"nested_option_2"}}
 
-echo '{{patch_options_json}}' | jq -c 'to_entries[]' | while read -r option_entry; do
-  option_name=$(echo "$option_entry" | jq -r '.key')
-  echo "$option_entry" | jq -c '.value | to_entries[]' | while read -r kv; do
+PATCH_OPTIONS_JSON="${PATCH_OPTIONS_JSON:-{{patch_options_json}}}"
+
+echo "$PATCH_OPTIONS_JSON" | jq -c 'to_entries[] | {option: .key, pairs: .value | to_entries}' | while read -r option_obj; do
+  option_name=$(echo "$option_obj" | jq -r '.option')
+  echo "$option_obj" | jq -c '.pairs[]' | while read -r kv; do
     key=$(echo "$kv" | jq -r '.key')
     value=$(echo "$kv" | jq -r '.value')
     echo "Running: wp option patch insert $option_name $key $value"
