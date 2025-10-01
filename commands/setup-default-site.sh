ec3f7a1b576cba3f548d583f9909b9e68c66b39c
Johan Falk <johan@krokedil.se>
2025-10-01 21:38:36 +0200

    Remove --skip-plugins and --skip-themes from WP CLI commands
    Eliminated the --skip-plugins and --skip-themes flags from various wp wc and wp import commands in helper and setup scripts. This change ensures that plugins and themes are not skipped during WooCommerce blueprint imports, payment gateway updates, tax creation, and shipping method setup.

--- commands/setup-default-site.sh
+++ commands/setup-default-site.sh
@@ -12,7 +12,7 @@ wp config set ALLOW_BLUEPRINT_IMPORT_IN_LIVE_MODE true --raw --skip-plugins --sk
 
 # Download WC blueprint JSON file, import it and delete it.
 curl -fsSL "https://raw.githubusercontent.com/krokedil/instawp-commands/refs/heads/main/assets/wc-blueprints/wc-blueprint-default.json" -o "wc-blueprint-imported-from-instawp-command.json"
-wp wc blueprint import "wc-blueprint-imported-from-instawp-command.json" --show-messages=all --user=1 --skip-plugins --skip-themes
+wp wc blueprint import "wc-blueprint-imported-from-instawp-command.json" --show-messages=all --user=1
 rm "wc-blueprint-imported-from-instawp-command.json"
 
 # Download WXR xml, import products and coupons and delete it.
@@ -22,11 +22,11 @@ wp import wxr-imported-from-instawp-command.xml --authors=skip --skip-plugins --
 rm "wxr-imported-from-instawp-command.xml"
 
 # Create tax settings and shipping methods.
-wp wc tax create --country="SE" --rate="25" --name="VAT 25%" --class="standard" --user=1 --skip-plugins --skip-themes
-wp wc tax create --country="SE" --rate="12" --name="VAT 12%" --class="reduced-rate" --user=1 --skip-plugins --skip-themes
-wp wc tax create --country="SE" --rate="0" --name="VAT 0%" --class="zero-rate" --user=1 --skip-plugins --skip-themes
-wp wc shipping_zone_method create 0 --enabled=true --method_id="flat_rate" --settings='{"title": "Home delivery","cost": "40,83"}' --user=1 --skip-plugins --skip-themes
-wp wc shipping_zone_method create 0 --enabled=true --method_id="local_pickup" --settings='{"title": "Pickup in store","cost": "0"}' --user=1 --skip-plugins --skip-themes
+wp wc tax create --country="SE" --rate="25" --name="VAT 25%" --class="standard" --user=1
+wp wc tax create --country="SE" --rate="12" --name="VAT 12%" --class="reduced-rate" --user=1
+wp wc tax create --country="SE" --rate="0" --name="VAT 0%" --class="zero-rate" --user=1
+wp wc shipping_zone_method create 0 --enabled=true --method_id="flat_rate" --settings='{"title": "Home delivery","cost": "40,83"}' --user=1
+wp wc shipping_zone_method create 0 --enabled=true --method_id="local_pickup" --settings='{"title": "Pickup in store","cost": "0"}' --user=1
 
 # Update user meta to simplify testing when logged in as admin.
 wp user meta add 1 billing_first_name "Krokedil" --skip-plugins --skip-themes
